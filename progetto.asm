00a994ba05bd9966ac949c3c3c365179abef3703
Alessandro <140234678+alepiace777@users.noreply.github.com>
2024-10-07 17:22:29 +0200

    Add files via upload

---
+++ progetto.asm
@@ -0,0 +1,7 @@
+# Prova del programma: vorrei leggere dei valori in codice gray,
+# convertirli in binario, e poi sommare i numeri uno in v.a.
+# e uno in c.a 2, infine confrontare i due valori e 
+# dire quale dei due `e maggiore, se uno dei due `e maggiore
+
+    .data 0x10010000
+numeri: .integer 43 55 23 84 a5 ff 78 45
