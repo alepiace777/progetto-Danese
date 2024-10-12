6ce3aba4396bd1017f3690f923382a4f44399db7
Ale <lelow@lol.it>
2024-10-12 18:41:11 +0200

    prime fasi

--- progetto.asm
+++ progetto.asm
@@ -1,9 +1,17 @@
 # Prova del programma: vorrei leggere dei valori in codice gray,
-# convertirli in binario, e poi sommare i numeri uno in v.a.
-# e uno in c.a 2, infine confrontare i due valori e 
-# dire quale dei due `e maggiore, se uno dei due `e maggiore
+# convertirli in binario, e poi sommare i numeri tra di loro,
+# infine confrontare i due valori e dire quale dei due `e maggiore,
+# se uno dei due `e maggiore
 
     .data 0x10010000
-numeri: .integer 43 55 23 84 a5 ff 78 45
+numeri: .integer 4 43 55 23 84 a5 ff 78 45
 
+
+# per convertire da gray a binaro ricopio il primo bit e compio
+# la xor tra il bit convertito precedentemente e quello in gray
+
+    .text 0x400400
+lbu $s0, numeri
+addi $s1, $0, 8
+lettura_val: lbu $s2, (numeri + $s1)
 
