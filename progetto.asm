75705641e1050692d8684fe96b3684b208febfac
Ale Desktop <lol@lul.it>
2024-10-23 22:27:55 +0200

    v3

--- progetto.asm
+++ progetto.asm
@@ -1,10 +1,11 @@
 # Prova del programma: vorrei leggere dei valori in codice gray,
 # convertirli in binario, e poi sommare i numeri tra di loro,
 # infine confrontare i due valori e dire quale dei due `e maggiore,
-# se uno dei due `e maggiore
+# se uno dei due `e maggiore, e salvare se sono ci sono piú maggiori
+# nella prima serie o nella seconda
 
     .data 0x10010000
-    .word 04
+    .word 0x4
 numeri: .byte 43, 55, 23, 84, a5, ff, 78, 45
 
 
@@ -12,14 +13,14 @@ numeri: .byte 43, 55, 23, 84, a5, ff, 78, 45
 # la xor tra il bit convertito precedentemente e quello in gray
 
     .text 0x400400
-#s0 indirizzo dati, $s1 num elementi
-
+#s0 indirizzo dati, $s1 num elementi, $s2 valore1, $s3 valore2
+#preimpostazione dati
 addiu $s0, $0, 0x10010000
-li $s1, 0($s0)
+lw $s1, 0($s0)
 addiu $s0, $s0, 4
-fetch: 
-lbu $s1, 0($s0)
-addi $s1, $0, 8
-lettura_val: lbu $s2, (numeri + $s1)
+#prelevamento dati effettivo
+fetch: lbu $s2, 0($s0)
+lbu $s3, 1($s0)
+add $s0, $s0, 2
 
-#decremento $s2
+#conversione gray-binario
