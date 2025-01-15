5b29b35aff0873a92f63e95134aa0673b7a483b8
Ale <lelow@lol.it>
2025-01-15 18:41:40 +0100

    ultimino

--- progetto2.asm
+++ progetto2.asm
@@ -1,12 +1,12 @@
-#programmi che potrei fare:
-#- fibonacci
-#- 
+# descrizione programma: calcolatore della
+# sequenza di Fibonacci a partire da un numero
+# inoltre verifica l'overflow
 
     .data 0x10010000
-val: .byte 0x2e
+val: .byte 0x44 #2e
 
 #s2, s3 registri usati per salvare i valori
-#s0 registro di quanti valori calcolare
+#s1 registro di quanti valori calcolare
 #t0 salvo il valore di s3 di partenza, che poi
 #andra` inserito in s2
 
@@ -14,7 +14,6 @@ val: .byte 0x2e
 .globl __start
 __start: la $s0, val
 lbu $s1, 0($s0)
-
 beq $s1, $0, fine
 
 li $s2, 1
@@ -24,8 +23,8 @@ li $s3, 1
 
 addiu $s1, $s1 -2
 
+#ciclo principale
 loop: beq $s1, $0, fine
-
 
 xor $t0, $s3, $0
 
@@ -33,8 +32,6 @@ addu $s3, $s2, $s3
 xor $s2, $t0, $0
 
 ble $s3, $s2, ovf
-
-#ow: xor, and, bne
 
 addiu $s1, $s1, -1
 
